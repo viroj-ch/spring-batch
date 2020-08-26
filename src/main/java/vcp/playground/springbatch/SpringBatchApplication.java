329db39882ff1ce2b6854574c63a54ac4d8af095
viroj-ch <drslamp@hotmail.com>
2020-08-26 22:47:54 +0700

    init commit

---
+++ src/main/java/vcp/playground/springbatch/SpringBatchApplication.java
@@ -0,0 +1,16 @@
+package vcp.playground.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+
+        System.exit(
+                SpringApplication.exit(
+                        SpringApplication.run(SpringBatchApplication.class, args)));
+    }
+
+}
